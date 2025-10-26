73b6411e629b069382f7cbdbe64907aaa94377ae
Raul Najera <tlacodevs@gmail.com>
2025-10-26 12:58:19 -0600

    fix: establece velocidad predeterminada y corrige dirección de movimiento del avión

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 30;
     public float rotationSpeed = 100;
     //public float verticalInput;
 
@@ -21,7 +21,7 @@ public class PlayerControllerX : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * -verticalInput);
